8f22db25cb8dece0364de46fb6f71ade6b2dec3b
Wildan Aizzaddin <aizzaddin@gmail.com>
2021-06-21 13:06:08 +0000

    Add Entity For Sprint 2

---
+++ src/main/java/com/synrgybootcamp/project/enums/MissionType.java
@@ -0,0 +1,14 @@
+package com.synrgybootcamp.project.enums;
+
+public enum MissionType {
+    TRANSFER,
+    POCKET,
+    PAYMENT_MERCHANT,
+    PAYMENT_MOBILE_CREDIT,
+    PAYMENT_PDAM,
+    PAYMENT_ELECTRICITY,
+    PAYMENT_TELEVISION,
+    PAYMENT_TRAIN,
+    PAYMENT_DONATION,
+    PAYMENT_INSURANCE
+}
