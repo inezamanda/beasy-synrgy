fde19e97797a904d6887a4828534448c0dda5f01
M. Firhan Azmi Nor <muhammad.firhan.azmi@gmail.com>
2021-07-01 21:58:46 +0700

    fix import class

--- src/main/java/com/synrgybootcamp/project/web/controller/GamificationUserController.java
+++ src/main/java/com/synrgybootcamp/project/web/controller/GamificationUserController.java
@@ -2,12 +2,12 @@ package com.synrgybootcamp.project.web.controller;
 
 import com.synrgybootcamp.project.service.GamificationUserService;
 import com.synrgybootcamp.project.util.ApiResponse;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
