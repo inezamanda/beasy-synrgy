1227622f17bad6a04036ea5fdcf82a93bcd4b2a9
Wildan Aizzaddin <aizzaddin@gmail.com>
2021-07-01 17:41:12 +0700

    Ada yang kelewat. seeting api di wallet adan accoun controller

--- src/main/java/com/synrgybootcamp/project/web/controller/AccountController.java
+++ src/main/java/com/synrgybootcamp/project/web/controller/AccountController.java
@@ -4,20 +4,26 @@ import com.synrgybootcamp.project.service.impl.AccountServiceImpl;
 import com.synrgybootcamp.project.util.ApiResponse;
 import com.synrgybootcamp.project.web.model.request.AccountRequest;
 import com.synrgybootcamp.project.web.model.response.AccountResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/accounts")
+@Api(tags = "Accounts", description = "Accounts Controller")
 public class AccountController {
     @Autowired
     AccountServiceImpl accountService;
 
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Get list of accounts")
     public ResponseEntity<ApiResponse> listAccounts(
             @RequestParam(required = false) String keyword
     ) {
@@ -28,6 +34,8 @@ public class AccountController {
     }
 
     @GetMapping("/recent")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Get recent account")
     public ResponseEntity<ApiResponse> recentAccounts(){
         List<AccountResponse> accounts = accountService.recentAccounts();
         return new ResponseEntity<>(
@@ -36,6 +44,8 @@ public class AccountController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Add new account")
     public ResponseEntity<ApiResponse> addAccount(@RequestBody AccountRequest accountRequest){
         AccountResponse account = accountService.createAccount(accountRequest);
         return new ResponseEntity<>(
@@ -44,6 +54,8 @@ public class AccountController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Get detail account")
     public ResponseEntity<ApiResponse> getAccountById(
             @PathVariable String id){
         AccountResponse detailAccount = accountService.getAccountById(id);
@@ -54,6 +66,8 @@ public class AccountController {
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Edit account")
     public ResponseEntity<ApiResponse> updateAccountById(
             @PathVariable String id,
             @RequestBody AccountRequest accountRequest){
@@ -64,6 +78,8 @@ public class AccountController {
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Delete Account")
     public ResponseEntity<ApiResponse> deleteAccountById(@PathVariable String id){
         boolean deleted = accountService.deleteAccountById(id);
 
--- src/main/java/com/synrgybootcamp/project/web/controller/EwalletController.java
+++ src/main/java/com/synrgybootcamp/project/web/controller/EwalletController.java
@@ -6,6 +6,8 @@ import com.synrgybootcamp.project.util.ApiResponse;
 import com.synrgybootcamp.project.web.model.request.EwalletTransactionRequest;
 import com.synrgybootcamp.project.web.model.response.EwalletResponse;
 import com.synrgybootcamp.project.web.model.response.EwalletTransactionResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/ewallet")
+@Api(tags = "TopUp Ewallet", description = "Ewallet controller")
 public class EwalletController {
     @Autowired
     private EwalletServiceImpl ewalletService;
@@ -25,6 +28,7 @@ public class EwalletController {
 
     @GetMapping
     @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Get list of ewallet")
     public ResponseEntity<ApiResponse> getAllEwallets() {
         List<EwalletResponse> ewallets = ewalletService.getAllEwallets(
         );
@@ -35,6 +39,8 @@ public class EwalletController {
     }
 
     @PostMapping()
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Top Up Ewallet")
     public ResponseEntity<Object> ewalletTransaction(@RequestBody EwalletTransactionRequest ewalletTransactionRequest) {
         EwalletTransactionResponse ewalletResult = ewalletTransactionService.ewalletTransaction(
                 EwalletTransactionRequest
