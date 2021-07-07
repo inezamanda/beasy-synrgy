62a932d08a6935b413b123c0dd6e49f8b489123b
Wildan Aizzaddin <aizzaddin@gmail.com>
2021-07-07 21:34:16 +0700

    Remove limit

--- src/main/java/com/synrgybootcamp/project/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/synrgybootcamp/project/service/impl/TransactionServiceImpl.java
@@ -38,7 +38,6 @@ public class TransactionServiceImpl implements TransactionService {
         List<Transaction> transactions = transactionRepository.findByUserAndTypeInOrderByDateDesc(user, transactionTypes);
         return transactions
                 .stream()
-                .limit(5)
                 .map(transaction -> RecentTransactionResponse
                 .builder()
                         .id(transaction.getId())
