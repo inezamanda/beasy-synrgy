bf642bdb03f4742e45c9c4343fcdc6937a637a1e
Inez Yulia Amanda <inez.amanda16@gmail.com>
2021-07-12 15:16:06 +0800

    get transfer history of user

--- src/main/java/com/synrgybootcamp/project/service/TransferService.java
+++ src/main/java/com/synrgybootcamp/project/service/TransferService.java
@@ -1,8 +1,13 @@
 package com.synrgybootcamp.project.service;
 
 import com.synrgybootcamp.project.web.model.request.TransferRequest;
+import com.synrgybootcamp.project.web.model.response.TransferHistoryResponse;
 import com.synrgybootcamp.project.web.model.response.TransferResponse;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 public interface TransferService {
     TransferResponse transfer(TransferRequest transferRequest);
+    List<TransferHistoryResponse> getHistory(Sort sort);
 }
