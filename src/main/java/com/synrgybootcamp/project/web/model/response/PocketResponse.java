4594df1204d75ab273bea0d92141cbd63b9a430f
Wildan Aizzaddin <aizzaddin@gmail.com>
2021-07-13 19:32:20 +0700

    Add json format to date.

--- src/main/java/com/synrgybootcamp/project/web/model/response/PocketResponse.java
+++ src/main/java/com/synrgybootcamp/project/web/model/response/PocketResponse.java
@@ -1,5 +1,6 @@
 package com.synrgybootcamp.project.web.model.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,15 +14,22 @@ import java.util.Date;
 @Builder
 @AllArgsConstructor
 public class PocketResponse {
+
     @JsonProperty("user_id")
     String userId;
+
     String id;
     String picture;
+
     @JsonProperty("pocket_name")
     String pocketName;
+
     Integer target;
     Integer balance;
     boolean primary;
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", timezone = "GMT+7")
     Date dueDate;
+
     boolean delete;
 }
