c3b519c67e256fc2426a0c5fc841a435499160cf
M. Firhan Azmi Nor <muhammad.firhan.azmi@gmail.com>
2021-07-15 20:55:01 +0700

    fix card information static data & recent transaction missing field

--- src/main/java/com/synrgybootcamp/project/util/ImageUtil.java
+++ src/main/java/com/synrgybootcamp/project/util/ImageUtil.java
@@ -5,22 +5,22 @@ import com.synrgybootcamp.project.enums.TransactionType;
 public class ImageUtil {
 
   public static String getImageFromTransactionType(TransactionType type) {
-    String image = "https://www.freepnglogos.com/uploads/logo-bca-png/bank-bca-logo-bca-aroma-incense-website-2.png";
+    String image = "";
     switch (type) {
       case EWALLET:
-        image = "https://www.freepnglogos.com/uploads/logo-bca-png/bank-bca-logo-bca-aroma-incense-website-2.png";
-        break;
+        image = "https://res.cloudinary.com/dh9nmeyfy/image/upload/v1626356436/e-wallet_yzbj6v.svg";
+      break;
       case TRANSFER:
-        image = "https://www.freepnglogos.com/uploads/logo-bca-png/bank-bca-logo-bca-aroma-incense-website-2.png";
+        image = "https://res.cloudinary.com/dh9nmeyfy/image/upload/v1626356432/transfer_nlktyf.svg";
         break;
       case PAYMENT_MOBILE:
-        image = "https://www.freepnglogos.com/uploads/logo-bca-png/bank-bca-logo-bca-aroma-incense-website-2.png";
+        image = "https://res.cloudinary.com/dh9nmeyfy/image/upload/v1626356586/mobile_amgr8o.svg";
         break;
       case PAYMENT_MERCHANT:
-        image = "https://www.freepnglogos.com/uploads/logo-bca-png/bank-bca-logo-bca-aroma-incense-website-2.png";
+        image = "https://res.cloudinary.com/dh9nmeyfy/image/upload/v1626356420/merchant_ywl1an.svg";
         break;
       case PAYMENT_CREDIT_CARD:
-        image = "https://www.freepnglogos.com/uploads/logo-bca-png/bank-bca-logo-bca-aroma-incense-website-2.png";
+        image = "https://res.cloudinary.com/dh9nmeyfy/image/upload/v1626356499/credit_card_pxcz5o.svg";
         break;
       default:
         break;
--- src/main/java/com/synrgybootcamp/project/web/model/response/sub/SubRecentTransactionResponse.java
+++ src/main/java/com/synrgybootcamp/project/web/model/response/sub/SubRecentTransactionResponse.java
@@ -19,4 +19,6 @@ public class SubRecentTransactionResponse {
 
   Date date;
 
+  Integer totalAmount;
+
 }
